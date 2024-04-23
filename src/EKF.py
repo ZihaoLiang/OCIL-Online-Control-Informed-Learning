6d9b60e78f6ca8055bebaf34b246d937e8136ba3
Zihao Liang <danieleung@Zihaos-MacBook-Pro-24.local>
2024-04-23 16:43:17 -0400

    EKF

--- src/EKF.py
+++ src/EKF.py
@@ -2,10 +2,28 @@ import numpy as np
 
 class EKF:
 
-    def __init__(self, theta_prev,):
+    def __init__(self):
+        return
 
-    def predict(self, theta_prev, ):
+    def predict(self, theta_prev, P_prev, Q_prev):
+        self.theta_ = theta_prev
+        self.P_ = P_prev + Q_prev
+        
+    def update(self, dLdtheta, R, stageLoss):
+        p = np.shape(dLdtheta) #size of theta
+        S = np.matmul(np.matmul(dLdtheta, self.P_), np.transpose(dLdtheta)) + R
+        kalmanGain = np.matmul(self.P_, dLdtheta) * 1/S #Kalman Gain
+        self.P = np.matmul((np.eye(p) - np.matmul(kalmanGain, dLdtheta)), self.P_) #Update P
+        self.theta = self.theta_ + np.matmul(kalmanGain, stageLoss) #Update theta
+        return self.P
+    
+if __name__ == '__main__':
+    theta_prev = np.array((1, 2, 3))
+    P_prev = np.array([[0.001, 0, 0], [0, 0.001, 0], [0, 0, 0.001]])
+    Q_prev = np.array([[0.001, 0, 0], [0, 0.001, 0], [0, 0, 0.001]]) 
+    dLdtheta = np.array((1, 2, 3)) 
+    R = 0.001
+    stageLoss = 0.1
 
-
-    def update(self):
-
+    updateTheta = EKF()
+    updateTheta.predict(theta_prev, P_prev, Q_prev)
