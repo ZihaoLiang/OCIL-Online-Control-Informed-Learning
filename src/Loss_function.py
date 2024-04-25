aed7c13f48016c1325f0dc953f0fab664ba8aa63
tianyuzhou-sam <zhou1043@purdue.edu>
2024-04-24 21:05:57 -0700

    Loss function

--- src/Loss_function.py
+++ src/Loss_function.py
@@ -1,14 +1,20 @@
 import numpy as np
-
+import casadi
 
 class Loss:
-    def _init_(self):
-        pass
+    def __init__(self, n, m):
+        self.xi = casadi.SX.sym("xi", n+m)
 
-    def getLoss(traj, traj_oc):
-        return np.linalg.norm(traj-traj_oc, ord=2)**2
+    def getLossValue(self, traj_t, traj_oc_t):
 
+        self.loss = casadi.norm_2(self.xi-traj_oc_t)**2
+        self.dLdXi = casadi.jacobian(self.loss, self.xi)
+        self.lossFun = casadi.Function("lossFun", [self.xi], [self.loss])
+        self.dLdXiFun = casadi.Function("dLdXiFun", [self.xi], [self.dLdXi])
 
+        lossNow = self.lossFun(traj_t).full()[0, 0]
+        dLdXiNow = self.dLdXiFun(traj_t).full()[0,0]
 
+        return lossNow, dLdXiNow
 
 
