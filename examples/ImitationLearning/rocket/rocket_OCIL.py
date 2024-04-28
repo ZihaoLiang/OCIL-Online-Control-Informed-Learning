2921f95c056d5da0bed5b167b32439b5cc08eb55
tianyuzhou-sam <zhou1043@purdue.edu>
2024-04-27 21:29:13 -0700

    Rocket example

---
+++ examples/ImitationLearning/rocket/rocket_OCIL.py
@@ -0,0 +1,35 @@
+import numpy as np
+from casadi import *
+import scipy.io as sio
+import os
+import sys
+sys.path.append(os.getcwd() + '/src')
+sys.path.append(os.getcwd() + '/externals/Pontryagin-Differentiable-Programming')
+from OCIL import OCIL
+from JinEnv import JinEnv
+
+
+
+# ------------------------------ Set up dynamic system ------------------------------
+project = "Rocket"
+mode = "Imitation Learning"
+saveFlag = False
+dynsys = JinEnv.Rocket()
+dynsys.initDyn(Jx=0.5, Jy=1, Jz=1, mass=1, l=1)
+dynsys.initCost(wthrust = 0.1)
+
+dir = 'examples/ImitationLearning/rocket/data/'
+demoFile = 'rocket_demos.mat'
+
+system = OCIL(project, mode, dynsys, dir, demoFile, saveFlag)
+
+# --------------------------- initilize EKF ----------------------------------------
+P = np.eye(5) * 0.0000001
+Q = np.eye(5) * 0.
+R = np.eye(16) * 0.0000000001
+
+system.initialize_EKF(P, Q, R)
+
+# system.solve()
+system.solveAllLoss()
+
