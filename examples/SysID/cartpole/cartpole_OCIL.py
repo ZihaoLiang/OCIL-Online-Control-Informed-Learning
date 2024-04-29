9f8c5c3a8103de579f76cf63cf472b70aa8d4894
tianyuzhou-sam <zhou1043@purdue.edu>
2024-04-28 18:33:17 -0700

    Add SysID examples

--- examples/SysID/cartpole/cartpole_OCIL.py
+++ examples/SysID/cartpole/cartpole_OCIL.py
@@ -11,22 +11,22 @@ from JinEnv import JinEnv
 
 
 # ------------------------------ Set up dynamic system ------------------------------
-project = "Quadrotor"
-mode = "Imitation Learning"
+project = "CartPole"
+mode = "SysID"
 saveFlag = False
-dynsys = JinEnv.Quadrotor()
-dynsys.initDyn(Jx=1, Jy=1, Jz=1, mass=1, l=0.4, c=0.01)
-dynsys.initCost(wthrust=0.1)
+dynsys = JinEnv.CartPole()
+dynsys.initDyn()
+dynsys.initCost(wx=1,wq=6,wdx=1,wdq=1,wu = 0.1)
 
-dir = 'examples/ImitationLearning/uav/data/'
-demoFile = 'uav_demos.mat'
+dir = 'examples/SysID/cartpole/data/'
+demoFile = 'cartpole_demos.mat'
 
 system = OCIL(project, mode, dynsys, dir, demoFile, saveFlag)
 
 # --------------------------- initilize EKF ----------------------------------------
-P = np.eye(4) * 0.0000001
-Q = np.eye(4) * 0.
-R = np.eye(17) * 0.0000000001
+P = np.eye(3) * 0.0000001
+Q = np.eye(3) * 0.
+R = np.eye(5) * 0.0000000001
 
 system.initialize_EKF(P, Q, R)
 
